e50ee3b3dafb7ef64af2ed3c67b9284cfdc155f7
Radhika gupta <67688493+RaDhikagUptaweB@users.noreply.github.com>
2024-03-22 21:20:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
