9a1c8a12a24129548f2ee723a84c1343d53fb993
Radhika gupta <67688493+RaDhikagUptaweB@users.noreply.github.com>
2024-03-23 23:47:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ let cards;
 let interval;
 let firstCard = false;
 let secondCard = false;
+
+const items = [
+    { name: "bee", image: "bee.png" },
+    { name: "crocodile", image: "crocodile.png" },
+    { name: "macaw", image: "macaw.png" },
+    { name: "gorilla", image: "gorilla.png" },
+    { name: "tiger", image: "tiger.png" },
+    { name: "monkey", image: "monkey.png" },
+    { name: "chameleon", image: "chameleon.png" },
+    { name: "piranha", image: "piranha.png" },
+    { name: "anaconda", image: "anaconda.png" },
+    { name: "sloth", image: "sloth.png" },
+    { name: "cockatoo", image: "cockatoo.png" },
+    { name: "toucan", image: "toucan.png" },
+  ];
