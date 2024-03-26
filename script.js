6532d03e30886eae65c4be6865af314b74b7b8cc
Radhika gupta <67688493+RaDhikagUptaweB@users.noreply.github.com>
2024-03-26 23:53:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ const items = [
     { name: "cockatoo", image: "cockatoo.png" },
     { name: "toucan", image: "toucan.png" },
   ];
+  let seconds = 0,
+  minutes = 0;
+//Initial moves and win count
+let movesCount = 0,
+  winCount = 0;
+//For timer
+const timeGenerator = () => {
+  seconds += 1;
+  //minutes logic
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+  //format time before displaying
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
