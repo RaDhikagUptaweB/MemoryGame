bbe70a68e5c8774cb5ca945faeafd39672aa5161
Radhika gupta <67688493+RaDhikagUptaweB@users.noreply.github.com>
2024-04-01 22:42:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -127,3 +127,18 @@ const matrixGenerator = (cardValues, size = 4) => {
         }
       });
     });
+    //Start game
+startButton.addEventListener("click", () => {
+  movesCount = 0;
+  seconds = 0;
+  minutes = 0;
+  //controls amd buttons visibility
+  controls.classList.add("hide");
+  stopButton.classList.remove("hide");
+  startButton.classList.add("hide");
+  //Start timer
+  interval = setInterval(timeGenerator, 1000);
+  //initial moves
+  moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+  initializer();
+});
